f57e8785bae35643ad014d03a78d9db3025976b3
Hugo Quiñónez <hugo_2555@hotmail.com>
2018-07-07 21:50:25 -0600

    Home, navbar and footer

---
+++ mariorentas/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './/app-routing.module';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+
+@NgModule({
+   declarations: [
+      AppComponent,
+      HomeComponent,
+      NavbarComponent,
+      FooterComponent
+   ],
+   imports: [
+      BrowserModule,
+      AppRoutingModule
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
+})
+export class AppModule { }
